7ad25756ec714e16a859402504d8713f8431d873
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-07-05 16:06:00 -0300

    Create migrations and DB connection

---
+++ db/migrations/20240705184759_create-meals.ts
@@ -0,0 +1,18 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.string('name').notNullable()
+    table.string('description').notNullable()
+    table.boolean('is_on_diet').notNullable()
+    table.date('date').notNullable()
+    table.uuid('user_id').references('users.id').notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
