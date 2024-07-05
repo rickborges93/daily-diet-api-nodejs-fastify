99eb9a3b61975888b21a6deb2835d3640126fec2
Henrique Samensari Borges <henriquesamensari@gmail.com>
2024-07-05 16:25:03 -0300

    users endpoint and types

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,13 @@
+import 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      name: string
+      email: string
+      session_id: string
+      created_at: string
+    }
+  }
+}
